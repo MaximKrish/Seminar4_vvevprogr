e3380e709a19c0d9f06bcb835ee989c310db2be0
Maxim <m.krishnevskiy@kmg.kz>
2022-07-04 17:49:08 +0600

    Zadacha24_

---
+++ Zadacha26/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(n > 0)
+{
+count ++;
+    n = n / 10;
+}
+  
+Console.WriteLine("Количество цифр равно:");
+Console.WriteLine(count);
+
+// Console.Write("ВВедите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine((int)Math.Log10(num)+1);
+
+// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
---
+++ Zadacha30/Program.cs
@@ -0,0 +1,26 @@
+﻿int [] array = new int[8];
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(0, 2); //8 длина массива
+        index++;
+    }
+}
+
+void PrintArray(int[]col){
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+FillArray(array);
+PrintArray(array);
+
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
