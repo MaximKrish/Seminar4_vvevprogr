d6f917b912220739260f976e8a7eabe91e25d46b
Maxim <m.krishnevskiy@kmg.kz>
2022-07-05 16:22:06 +0600

    dz27 , 29

---
+++ DZ27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+
+
+Console.WriteLine("Сумма цифр в числе: ");
+Console.WriteLine(sum);
---
+++ DZ29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] col = new int[8];
+Console.WriteLine("Введи цеферки");
+for (int index = 0; index <8; index++)
+    {
+        col[index] = Convert.ToInt32(Console.ReadLine()); //8 длина массива
+    }
+    Console.Write("["+col[0]+",");
+for (int index = 1; index <7; index++)
+    {
+        Console.Write(col[index]+",");
+    }
+    Console.Write(col[7]+"]");
