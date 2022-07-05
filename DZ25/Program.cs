de58268b73d07c2c6adcc5c4822bcee02f0c4b03
Maxim <m.krishnevskiy@kmg.kz>
2022-07-05 14:18:08 +0600

    dz25

---
+++ DZ25/Program.cs
@@ -0,0 +1,11 @@
+﻿int n, n1, sum =1;
+Console.WriteLine("Введите число A: ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n1; i++)
+{
+    sum = sum * n;
+}
+
+Console.WriteLine(sum);
