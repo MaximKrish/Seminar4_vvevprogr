c70dd0755826847106cb741f2dcd32d5e2a265c6
Maxim <m.krishnevskiy@kmg.kz>
2022-07-05 14:09:05 +0600

    Zadacha30_

--- Zadacha30_/Program.cs
+++ Zadacha30_/Program.cs
@@ -1,17 +1,24 @@
 ﻿Console.WriteLine("Загадай число от 1 до 100");
-int n_first = 0, n_last = 100, mid = 50;
+int n_first = 0, n_last = 100, mid = 0;
 while (n_first <= n_last)
 {
     mid = (n_first + n_last) / 2;
     Console.WriteLine("Число больше {0}?", mid);
-    string ?ans = Console.ReadLine();
-    if (mid == n_first)
-        break;
-    if (ans == "yes"){
+    string? ans = Console.ReadLine();
+    if (ans == "yes")
+    {
         n_first = mid;
     }
-    else{
+    else
+    {
         n_last = mid;
     }
+    if (n_last - n_first == 1)
+        break;
+    
 }
+
+
 Console.WriteLine(mid);
+
+//находим число в промежутке от 1 до 100
